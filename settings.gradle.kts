69d64ffcfdebdce295e08dcdad153a4e213e11ef
microllate <120251959+microllate@users.noreply.github.com>
2026-09-04 22:00:15 +0800

    build: migrate settings to Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://api.xposed.info/") {
+            content {
+                includeGroup("de.robv.android.xposed")
+            }
+        }
+    }
+}
+
+rootProject.name = "BatteryTempStatus"
+include(":app")
